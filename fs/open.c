37eb7bd83e61aa8e1d714fb9edf60874bd348e28
raditows <11974771+radito@users.noreply.github.com>
2026-09-23 19:24:55 +0700

    kernelsu: integrate v3.2.0 legacy manual hooks

--- fs/open.c
+++ fs/open.c
@@ -353,6 +353,14 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
+#ifdef CONFIG_KSU
+	extern int ksu_handle_faccessat(int *dfd,
+					const char __user **filename_user,
+					int *mode, int *flags);
+
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
+
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
 
