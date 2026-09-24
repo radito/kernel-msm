1b560001fcf2daa23b0a9cdeaf37d2e02247bd9e
raditows <11974771+radito@users.noreply.github.com>
2026-09-24 07:49:56 +0700

    kernelsu: wire manual AVC audit spoof hook

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -770,6 +770,12 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	extern int ksu_handle_slow_avc_audit(u32 *tsid);
+
+	ksu_handle_slow_avc_audit(&tsid);
+#endif
+
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
